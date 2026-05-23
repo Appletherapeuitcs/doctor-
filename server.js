a35d5b058bc39821faa1daec3444d5abaef0ab10
Appletherapeuitcs <digital@appletherapeutics.com>
2026-05-23 14:20:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // PASTE YOUR MONGODB CONNECTION STRING HERE
-const uri = "mongodb+srv://digital_db_user:HlFvwmTpdPJJioA4@cluster0.cmdl06a.mongodb.net/?appName=Cluster0"; 
+const uri = "mongodb+srv://digital_db_user:Apple2026@cluster0.cmdl06a.mongodb.net/?appName=Cluster0"; 
 const client = new MongoClient(uri);
 
 let db;
